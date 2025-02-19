891e1ee6f51bda2ccb8134095eb002a890926ded
antfats <antfats@hotmail.com>
2025-02-19 02:35:37 -0700

    workin vercel version finally

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ const Home = () => {
           {/* text */}
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span>Developer <span className="text-accent">/</span> Security Specialist</span>
-            <h1 className="h1 mb-6">Hello My Name's<br /><span className="text-accent">Anthony Fatato</span>
+            <h1 className="h1 mb-6">Hello I&apos;m<br /><span className="text-accent">Anthony Fatato</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">I excel in maintaining security posture as well as creating dynamic websites and applications </p>
             {/* button and socials */}
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,18 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
-  {
-    rules: {
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
-      'react/no-unescaped-entities': 'off', // Disable the rule
-
-      // You can add more rules here if needed
-
-    },
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+      "react/no-unescaped-entities", "off"
+  
 ];
+
+export default eslintConfig;
